ba0237dcf8893620b1ac1ffb30ea056d9020ebf3
nathan-B04 <nathan.bricknell@dcmail.ca>
2025-03-31 19:22:03 -0400

    Update main.js

--- Assignment4/part2/gallery-start/main.js
+++ Assignment4/part2/gallery-start/main.js
@@ -1,3 +1,6 @@
+// Name: Nathan Bricknell
+// Date: March 31, 2025
+
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
@@ -5,6 +8,14 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const fileNamesArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', "pic4.jpg", 'pig5.jpg']
+const AltNames = {
+    'pic1.jpg': 'Picture 1',
+    'pic2.jpg': 'Picture 2',
+    'pic3.jpg': 'Picture 3',
+    'pic4.jpg': 'Picture 4',
+    'pic5.jpg': 'Picture 5'
+};
 
 /* Declaring the alternative text for each image file */
 
