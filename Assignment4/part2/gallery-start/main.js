61a861841f60c7f8673f35660fce311c9d68a640
nathan-B04 <nathan.bricknell@dcmail.ca>
2025-03-31 20:10:13 -0400

    Update main.js

--- Assignment4/part2/gallery-start/main.js
+++ Assignment4/part2/gallery-start/main.js
@@ -8,7 +8,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const fileNamesArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', "pic4.jpg", 'pig5.jpg']
+const fileNamesArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', "pic4.jpg", 'pic5.jpg']
 
 /* Declaring the alternative text for each image file */
 const AltNames = {
