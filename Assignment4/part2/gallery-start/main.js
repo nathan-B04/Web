fa784f0e31e73a25a1ed52cfd1a0ac95666b6772
nathan-B04 <nathan.bricknell@dcmail.ca>
2025-03-31 20:20:38 -0400

    Update main.js

--- Assignment4/part2/gallery-start/main.js
+++ Assignment4/part2/gallery-start/main.js
@@ -34,3 +34,15 @@ for (const pictures of fileNamesArray) {
   
 /* Wiring up the Darken/Lighten button */
 
+btn.addEventListener('click', () => {
+    if (btn.getAttribute('class') === 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColour = 'rgba(0,0,0,0.5)';
+    }
+    else {
+        btn.setAttribute('class', 'dark');
+        btn.textContent = "Darken";
+        overlay.style.backgroundColour = 'rgba(0,0,0,0)';
+    }
+})
